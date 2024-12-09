38b494efea8f732ec5010bc7acfa8b52f8b408f3
mutabazi-00 <mutajean30@gmail.com>
2024-12-09 15:59:16 +0200

    Fix random choosing
    The program will choose randomly using Math.random()

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 function getComputerChoice () {
-    
+
+    let randomNumber = Math.random();
+
+    if (randomNumber < 0.33) {
+        return "rock";
+    } else if (randomNumber < 0.66) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
 }
+
+console.log(getComputerChoice());
